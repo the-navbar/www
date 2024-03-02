22e5cf4c40e2b3fca278d8b61a3bc263bdf076d3
Simon Vrachliotis <simon@simonswiss.com>
2024-03-02 22:13:11 +1100

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import keystaticAstro from "@keystatic/astro";
 import htmx from "astro-htmx";
-
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://navbar.tech",
-  integrations: [tailwind(), react(), mdx(), htmx(), keystaticAstro()],
+  integrations: [tailwind(), react(), mdx(), htmx(), keystaticAstro(), alpinejs()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel()
 });
