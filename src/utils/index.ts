b6f026c7202dc5ee63a03ae57a3a63594f10b3ca
Simon Vrachliotis <simon@simonswiss.com>
2024-03-15 16:06:55 +1100

    Can't get the persisted page transitions working, so I turned it off for now. Would love some help!

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,20 @@
+import { CollectionEntry } from "astro:content";
+
 export * from "./get-latest-shows";
 
 // Allow merging of class strings (supporting Prettier sorting)
 export function cx(...classes: string[]): string {
   return classes.filter(Boolean).join(" ");
+}
+
+// Pretty date formatting
+export function getPrettyDate(
+  date: CollectionEntry<"shows">["data"]["publishedOn"],
+  month: Intl.DateTimeFormatOptions["month"] = "long",
+): string {
+  return new Date(date).toLocaleDateString("au-AU", {
+    year: "numeric",
+    month,
+    day: "numeric",
+  });
 }
